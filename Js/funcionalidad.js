ae8d363a87c6f55e999ebcb69c35c0cba478e62e
Pedro Gómez <pedrogomezzambra182@gmail.com>
2025-05-01 16:28:51 -0400

    agregue dos datos mas bailarines, agregue el logo a la ventana, elimine el estas listo, no necesario

--- Js/funcionalidad.js
+++ Js/funcionalidad.js
@@ -1,5 +1,4 @@
 window.onload = function() {
-
     const texts = [
         "El desperdicio alimentario genera aproximadamente 8-10% de las emisiones mundiales de gases de efecto invernadero",
         "Más de 820 millones de personas sufren inseguridad alimentaria en el mundo",
@@ -9,12 +8,31 @@ window.onload = function() {
         "La contaminación del aire causa alrededor de 7 millones de muertes prematuras al año",
         "Cada año, más de 8 millones de toneladas de plástico terminan en los océanos",
         "El 40% de la tierra agrícola en el mundo se destina a producir alimentos que nunca se consumen",
+        "Se necesitan 15.000 litros de agua para producir 1 kg de carne de res.",
+        "Más de 500 zonas muertas se han detectado en océanos y ríos por exceso de nutrientes y contaminación",
+        "Cada minuto se vende 1 millón de botellas de plástico, de las cuales menos del 10% se reciclan adecuadamente",
+        "La producción de alimentos es la principal causa de deforestación, pérdida de biodiversidad y degradación del suelo.",
     ];
 
-    const randomText = texts[Math.floor(Math.random() * texts.length)];
+    const elements = document.querySelectorAll(".movable-text");
 
-    document.getElementById("movable-text").innerText = randomText;
+
+    for (let i = texts.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [texts[i], texts[j]] = [texts[j], texts[i]];
+    }
+
+
+    elements.forEach((el, index) => {
+        if (index < texts.length) {
+            el.innerText = texts[index];
+        } else {
+            el.innerText = "Sin más datos disponibles"; 
+        }
+    });
 };
+
+
 
 
 
